39c37d9d90ea6610a5e8882f7b3d6eb9c5fe6c7d
Fabiola <fabi.ampuero.chalco@gmail.com>
2017-01-28 02:01:29 -0500

    se agrego mensajes de validacion en select

--- js/main.js
+++ js/main.js
@@ -17,7 +17,64 @@ var validator={
 			isValid= false;
 		}
 		return isValid;
-	}
+	},
+    isSelected: function(_value){
+        var isValid= true;
+        if(_value == 0){
+            isValid= false;
+        }
+        return isValid;
+    }
+}
+
+function validateForm() {
+	var name=document.getElementById("name");
+	var lastName=document.getElementById("lastname");
+	var email=document.getElementById("input-email");
+	var password=document.getElementById("input-password");
+    
+    var select= document.getElementsByTagName("select")[0];
+    console.log(select.value);
+
+	// -------------------------------- validacion de texto NOMBRE
+    if(validator.isText(name.value)){
+        removeMessage("name");
+    }
+    else{
+        createMessage("name","No es válido");
+    }
+    
+    // -------------------------------- validacion de texto APELLIDO
+    if(validator.isText(lastName.value)){
+        removeMessage("lastname");
+    }
+    else{
+        createMessage("lastname","No es válido");
+    }
+    
+    // -------------------------------- validacion de EMAIL
+    if(validator.isEmail(email.value)){
+        removeMessage("input-email");
+    }
+    else{
+        createMessage("input-email","No es válido");
+    }
+    
+    // -------------------------------- validacion de CONTRASEÑA
+    if(validator.isPassword(password.value)){
+        removeMessage("input-password");
+    }
+    else{
+        createMessage("input-password","No es válido");
+    }
+    
+    // -------------------------------- validacion de SELECCION
+    if(validator.isSelected(select.value)){
+        removeMessage("select");
+    }
+     else{
+        createMessage("select","No es válido");
+    }
 }
 
 function createMessage(_inputId, _message){
@@ -29,21 +86,25 @@ function createMessage(_inputId, _message){
 		span.innerHTML= _message;
 		elemento.parentNode.appendChild(span);
 	}
+    else{
+		//El span ya existe
+		if(elemento.nextSibling.tagName == 'span'){
+			elemento.nextSibling.innerHTML = _message;
+		}
+        else{
+			elemento.parentNode.removeChild(elemento.nextSibling);
+			var span = document.createElement('span');
+			span.innerHTML = _message;
+			elemento.parentNode.appendChild(span);
+		}
+	}
 }
 
 function removeMessage(_inputId){
     var elemento= document.getElementById(_inputId);
+    // si es que existe el span
     if(elemento.nextSibling != null){
         elemento.parentNode.removeChild(elemento.nextSibling);
     }
-}
-
-
-function validateForm(){
-	var name=document.getElementById("name");
-	var lastName=document.getElementById("lastname");
-	var email=document.getElementById("input-email");
-	var password=document.getElementById("input-password");
-	alert("oli");
 }
 
